c57da39479711780e547057ca957abf5443c26e2
KirillNemtyrev <nemtyrev3857@gmail.com>
2022-12-24 01:29:32 +0300

    Nikita pidor

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,26 +6,15 @@ module com.application.launcher {
     requires retrofit2.converter.gson;
 
     requires com.google.gson;
-    requires org.apache.httpcomponents.httpclient;
-    requires org.apache.httpcomponents.httpcore;
-    requires org.apache.httpcomponents.httpmime;
     requires java.desktop;
     requires okhttp3;
 
     opens com.application.launcher.rest.request to com.google.gson;
     opens com.application.launcher.rest.response to com.google.gson;
 
-    opens com.application.launcher.dto to com.google.gson;
-    exports com.application.launcher.dto;
-
-    opens com.application.launcher.entity to com.google.gson;
-    exports com.application.launcher.entity;
-
     opens com.application.launcher.controller to javafx.fxml;
     exports com.application.launcher.controller;
 
     opens com.application.launcher to javafx.fxml;
     exports com.application.launcher;
-    exports com.application.launcher.utils;
-    opens com.application.launcher.utils to javafx.fxml;
 }
